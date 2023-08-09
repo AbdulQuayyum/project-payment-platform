2ae37668b1778b5c74aa3017156f7167dd46f436
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-09 14:24:31 +0100

    Fixing branch error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,2 +1,13 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast';
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+      App
+    </>
+  )
+}
 
 export default App
