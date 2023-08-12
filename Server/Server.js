9b2e496013def60d7b311a15608a4c49f2a6bfa0
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-12 19:07:11 +0100

    Added cors to server side codebase...

--- Server/Server.js
+++ Server/Server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require("cors");
 require('dotenv').config()
 
 const DatabaseConfiguration = require("./Configurations/Database.Configuration")
@@ -7,6 +8,7 @@ const UserRoute = require("./Routes/User.Routes")
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(cors());
 app.use("/v1/Users", UserRoute)
 
 app.listen(port, () => console.log(`Project running on port ${port}!`))
