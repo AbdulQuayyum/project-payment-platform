531c54717128a6c21ff3241fee60a0d4a19745b9
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-12 18:28:32 +0100

    Added Authentication APIs...

--- Server/Server.js
+++ Server/Server.js
@@ -2,10 +2,11 @@ const express = require('express')
 require('dotenv').config()
 
 const DatabaseConfiguration = require("./Configurations/Database.Configuration")
+const UserRoute = require("./Routes/User.Routes")
 
 const app = express()
-
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('Heyy There!'))
+app.use("/v1/Users", UserRoute)
+
 app.listen(port, () => console.log(`Project running on port ${port}!`))
