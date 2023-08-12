2fd594d2adce227c1e14259bdc40f38a92ef6175
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-12 17:38:47 +0100

    Connected Project to a Database...

--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+require('dotenv').config()
+
+const DatabaseConfiguration = require("./Configurations/Database.Configuration")
+
 const app = express()
-const port = 8080
+
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send('Heyy There!'))
 app.listen(port, () => console.log(`Project running on port ${port}!`))
