002156cf67590aaf199b44b0a19d5468577db487
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-17 19:59:42 +0100

    Added Transaction Model...

---
+++ Server/Models/Transaction.Model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+    Amount: {
+        type: Number,
+        required: true,
+    },
+    Sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
+    },
+    Receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
+    },
+    Type: {
+        type: String,
+        required: true,
+    },
+    Reference: {
+        type: String,
+        required: true,
+    },
+    Status: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Transactions", TransactionSchema)
