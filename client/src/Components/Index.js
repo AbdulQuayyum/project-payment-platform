2ea24357e95b3ead148df709d718bec903813733
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-17 18:40:16 +0100

    Added a dynamic page title component...

--- client/src/Components/Index.js
+++ client/src/Components/Index.js
@@ -3,8 +3,11 @@ import Navbar from "./Auth/Navbar";
 
 import DashboardNavbar from "./Dashboard/Navbar";
 import Sidebar from "./Dashboard/Sidebar";
+import PageTitle from "./Dashboard/PageTitle";
+
 import Button from "./Dashboard/NavbarInfo/Button";
 import Notification from "./Dashboard/NavbarInfo/Notification";
 import UserProfile from "./Dashboard/NavbarInfo/UserProfile";
 
-export { Footer, Navbar, DashboardNavbar, Sidebar, Button, Notification, UserProfile }
+
+export { Footer, Navbar, DashboardNavbar, Sidebar, Button, Notification, UserProfile, PageTitle }
