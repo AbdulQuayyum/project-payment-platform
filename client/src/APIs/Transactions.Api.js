e3736dc834493c8f63468bfa3b72de293c467437
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-18 03:07:49 +0100

    Implementing VerifyAccount API...

--- client/src/APIs/Transactions.Api.js
+++ client/src/APIs/Transactions.Api.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { AxiosInstance } from "./Main";
 
-const TransactionsApi = () => {
-    return (
-        <div>TransactionsApi</div>
-    )
+// Verify Receiver's Account
+export const VerifyAccount = async (payload) => {
+    try {
+        const { data } = await AxiosInstance.post(`${import.meta.env.VITE_SERVER_URL}/v1/Transactions/VerifyAccount`, payload)
+        return data
+    } catch (error) {
+        return error.response.data
+    }
 }
-
-export default TransactionsApi
