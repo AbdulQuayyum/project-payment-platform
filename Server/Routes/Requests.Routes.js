0a959a6ee8626577978c001950a4cbc82f029ba4
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-24 03:51:50 +0100

    Working on the Requests APIs...

---
+++ Server/Routes/Requests.Routes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router()
+
+const AuthMiddleware = require("../Middlewares/Auth.Middleware")
+const Requests = require("../Models/Requests.Model")
+const User = require("../Models/User.Model")
+
+// Get all requests for a user
+router.post("/GetAllRequestsByUser", async (req, res) => {
+    try {
+        const NewRequests = await Requests.find({
+            $or: [{ Sender: req.User._id }, { Receiver: req.User._id }]
+        })
+            .populate("Sender")
+            .populate("Receiver")
+
+        res.send({
+            data: NewRequests,
+            message: "Request fetched successfully",
+            success: true
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+// Send request to another user 
+router.post("/SendRequest", async (req, res) => {
+    try {
+        const { Receiver, Amount, Reference } = req.body
+        const NewRequest = new Request({ Sender: req.User._id, Receiver: Receiver, Amount: Amount, Reference: Reference })
+
+        await NewRequest.save()
+
+        res.send({
+            data: NewRequest,
+            message: "Request sent successfully",
+            success: true
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+module.exports = router
--- Server/Server.js
+++ Server/Server.js
@@ -7,13 +7,15 @@ require('dotenv').config()
 const DatabaseConfiguration = require("./Configurations/Database.Configuration")
 const UserRoute = require("./Routes/User.Routes")
 const TransactionRoute = require("./Routes/Transactions.Routes")
+const RequestRoute = require("./Routes/Requests.Routes")
 
 const app = express()
 const port = process.env.PORT || 8080
 
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use("/v1/Users", UserRoute)
 app.use("/v1/Transactions", TransactionRoute)
+app.use("/v1/Requests", RequestRoute)
 
 app.listen(port, () => console.log(`Project running on port ${port}!`))
