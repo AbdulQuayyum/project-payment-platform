0cb94cb91b3d6439f1f6e9ae80e3310ef1ef3b28
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-24 03:25:56 +0100

    Working on the Requests APIs...

--- client/src/APIs/Request.Api.js
+++ client/src/APIs/Request.Api.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { AxiosInstance } from "./Main";
 
-const RequestApi = () => {
-    return (
-        <div>RequestApi</div>
-    )
+// Get All Requests for a User
+export const GetAllRequestsByUser = async () => {
+    try {
+        const { data } = await AxiosInstance.post(`${import.meta.env.VITE_SERVER_URL}/v1/Requests/GetAllRequestsByUser`)
+        return data
+    } catch (error) {
+        return error.response.data
+    }
 }
 
-export default RequestApi
+// Send a Request to another user
+export const SendRequest = async (request) => {
+    try {
+        const { data } = await AxiosInstance.post(`${import.meta.env.VITE_SERVER_URL}/v1/Requests/SendRequest`, request)
+        return data
+    } catch (error) {
+        return error.response.data
+    }
+}
