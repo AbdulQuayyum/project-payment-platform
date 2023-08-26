545c104ecfacc0381f25c84ed6f9bb49a5be4d19
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-26 09:10:16 +0100

    Working on implementing loader...

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BeatLoader } from 'react-spinners';
 
-const Loader = ({ loaderStatus }) => {
+const Loader = (props) => {
     return (
         <div>
             <BeatLoader
-                color="#0065AB"
-                loading={loaderStatus}
+                color="#000"
+                loading={true}
                 className="fixed top-0 left-0 right-0 bottom-0 w-screen h-screen z-50 overflow-hidden bg-black bg-opacity-75 transition-opacity !flex !flex-row items-center justify-center" />
         </div>
     )
