cd263ec8348cfaa40405fd4624af6072eae93e8c
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-08-27 17:08:12 +0100

    Working on the Footer...

--- client/src/Components/HomeFooter.jsx
+++ client/src/Components/HomeFooter.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const HomeFooter = () => {
+    const currentYear = new Date().getFullYear()
+
     return (
-        <div>HomeFooter</div>
+        <div className='footer-container'>
+            <div>
+                <span className=" dark:text-[#fff]">{currentYear} &#169; Na Me, All rights reserved</span>
+            </div>
+            <div className='flex gap-6'>
+                <span className="cursor-pointer dark:text-[#fff]">Terms & Conditions</span>
+                <span className="cursor-pointer dark:text-[#fff]">Privacy Policy</span>
+            </div>
+        </div>
     )
 }
 
